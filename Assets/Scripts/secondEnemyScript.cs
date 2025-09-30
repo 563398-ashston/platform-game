33f41f465cd742c2d23153409ba1813a16d9d8ee
563398-ashton <563398@escg.ac.uk>
2025-09-30 12:10:06 +0100

    added enemy 2

--- Assets/Scripts/secondEnemyScript.cs
+++ Assets/Scripts/secondEnemyScript.cs
@@ -1,21 +1,84 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class secondEnemyScript : MonoBehaviour
+public class SecondEnemyScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    { }
-    public LayerMask groundLayerMask;
+    float directionX;
+    public LayerMask wallLayerMask;
     Animator anim;
     Rigidbody2D rb;
     float xvel;
     Vector3 localScale;
 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        wallLayerMask = LayerMask.GetMask("wall");
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        xvel = 5;
+    }
+    
+
     // Update is called once per frame
     void Update()
     {
-        localScale = transform.localScale;
-        rb = GetComponent<Rigidbody2D>();
-        xvel = 10;
+        if (xvel < 0)
+        {
+            print("I am moving left");
+           // anim.SetBool("isWalking", true);
+
+            if (ExtendedRayCollisionCheck(-1, 0) == true)
+            {
+                xvel = 5;
+                
+            }
+        }
+
+        if (xvel > 0)
+        {
+            print("I am moving right");
+            //anim.SetBool("isWalking", true);
+
+            if (ExtendedRayCollisionCheck(1, 0) == true)
+            {
+                xvel = -5;
+                
+            }
+        }
+
+
+        //gameObject.transform.localScale = new Vector3(-1, 1, 1);
+
+        rb.linearVelocity = new Vector2(xvel, 0);
+    }
+
+    public bool ExtendedRayCollisionCheck(float xoffs, float yoffs)
+    {
+        float rayLength = 0.5f; // length of raycast 
+        bool hitSomething = false;
+
+        //convert x and y offset into a Vector 3
+        Vector3 offset = new Vector3(xoffs, yoffs, 0);
+
+        //cast a ray downwards 
+        RaycastHit2D hit;
+
+
+        hit = Physics2D.Raycast(transform.position + offset, -Vector2.up, rayLength, wallLayerMask);
+
+        Color hitColor = Color.white;
+
+        if (hit.collider != null)
+        {
+            print("player has collided with a wall");
+            hitColor = Color.green;
+            hitSomething = true;
+        }
+
+        Debug.DrawRay(transform.position + offset, Vector2.down * rayLength, hitColor);
+        return hitSomething;
     }
 }
